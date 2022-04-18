4b0fdd795f53db0b787dd722fba5225d8f1ebdef
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-04-17 22:58:07 -0500

    Se crea formulario dinamico basico

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  persona: Persona = {
+    nombre: 'Alejandra',
+    favoritos: [
+      { id: 1, nombre: 'Halo'},
+      { id: 2, nombre: 'Hercules'},
+      { id: 3, nombre: 'PES'},
+    ]
+  };
 
-  ngOnInit(): void {
+  guardar(): void {
+    console.log('Form Posteado');
   }
 
+  eliminar(indice: number): void {
+    this.persona.favoritos.splice(indice, 1);
+  }
 }
