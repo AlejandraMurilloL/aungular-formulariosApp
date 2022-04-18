9472dec9ba4dcdc29b81c462b3761811abd34b50
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-04-17 23:33:04 -0500

    Validaciones checks, radios, switches

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
-  constructor() { }
+  terminosYCondiciones: boolean = false;
 
-  ngOnInit(): void {
-  }
-
+  persona = {
+    genero: '',
+    notificaciones: true
+  };
 }
