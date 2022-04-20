880e44cd2483f2f3e148ca086f18c8e85249dda3
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-04-20 18:41:46 -0500

    Validacion personalizada llamando a funcion

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -13,16 +13,31 @@ export class RegisterComponent implements OnInit {
   nombreApellidoPettern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
   emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
+  noPuedeSerStrider(control: FormControl) {
+    const valor = control.value?.trim().toLowerCase();
+    if (valor === 'strider') {
+      return {
+        noStrider: true
+      }
+    }
+
+    return null;
+  }
+
+
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPettern)]],
     email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+    username: ['', [Validators.required, this.noPuedeSerStrider]],
   });
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
-      nombre: 'Alejandra Murillo'
+      nombre: 'Alejandra Murillo',
+      email: 'ale@gmail.com',
+      username: 'ale'
     });
   }
 
