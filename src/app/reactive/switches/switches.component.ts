e19dc6df04ad4681105aa194e8f4d32ac1adf609
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-04-20 16:26:10 -0500

    Mantener sincronizado valores del form con las propiedades de un objeto

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.formBuilder.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    terminos: [false, Validators.requiredTrue]
+  });
 
-  ngOnInit(): void {
+  persona = {
+    genero: 'F',
+    notificaciones: true
   }
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    //this.miFormulario.reset(this.persona);
+    this.miFormulario.reset({ 
+      ...this.persona, // Para enviar todas las propiedades que tiene persona, mas las que yo quiera añadir
+      terminos: false
+    });
+
+    this.miFormulario.valueChanges.subscribe(({terminos, ...rest}) => {
+      // Asi hacemos desestructuracion, obteniendo por un lado los terminos y por otro las demas propiedades ...rest
+      this.persona = rest;
+    });
+  }
+
+  guardar(): void {
+    const formValue = {...this.miFormulario.value};
+    delete formValue.terminos;
+    console.log(formValue);
+
+    this.persona = formValue;
+  }
 }
